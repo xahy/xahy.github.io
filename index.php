e316f3be9c51b83120a0838d238072554c34a903
Ismail Zahee <m3talloid@gmail.com>
2015-12-18 14:55:54 +0800

    move files

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>xahy.me</title>
+	<meta name="viewport" content="width=device-width, maximum-scale=1.5, minimal-ui" />
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/animate.css">
+	<link rel="stylesheet" href="css/app.css?=<?=microtime()?>">
+
+</head>
+<body>
+	
+	<section id="sec0">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12" style="padding-top:25%;">
+					<div id="pp" class="anim anim_0"></div>
+					<h1 id="headline" class="anim anim_1">Hi, I’m Zahee <br> I design and develop web / mobile Apps.</h1>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="sec1">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="anim anim_0">Projects I have worked</h2>
+				</div>
+				
+				<div class="col-md-7">
+					<div class="browser anim anim_1">
+						<div class="addressbar">
+							
+						</div>
+						<div class="browserbody">
+							
+						</div>
+					</div>
+				</div>
+				<div class="col-md-5">
+					<div class="anim anim_2">
+						<h3>Sun.mv</h3>
+						<div class="cont">
+							<p>Completely re-written the whole codebase of frontend and backend in MVC pattern that led to increase in performance and better code management of the Application<br><br>
+
+	New Modules such as Tag Management System, Article Management System (Journalists, Editors access level) were introduced in the process of building the CMS.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="sec2">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="anim anim_0">Get in touch</h2>
+					
+					<div class="anim anim_1">
+						<form action="#" id="get_in_touch_form" class="form-horizontal">
+							<div class="form-group">
+							    <label for="name-field" class="col-md-4 control-label">Full Name</label>
+							    <div class="col-md-4">
+							      	<input type="text" class="form-control" name="name" id="name-field" placeholder="" required>
+							    </div>
+						 	 </div>
+							<div class="form-group">
+							    <label for="email-field" class="col-md-4 control-label">Email</label>
+							    <div class="col-md-4">
+							      	<input type="email" class="form-control" name="email" id="email-field" placeholder="" required>
+							    </div>
+						 	 </div>
+						 	 <div class="form-group">
+							    <label for="message-field" class="col-md-4 control-label">Message</label>
+							    <div class="col-md-4">
+							      	<textarea name="message" id="message-field" rows="7" class="form-control" required placeholder=""></textarea>
+
+							    </div>
+						 	 </div>
+						 	 <div class="form-group">
+							    <label for="" class="col-md-4 control-label"></label>
+							    <div class="col-md-4">
+							      	<input type="submit" value="SEND" id="gettouchbtn">
+							    </div>
+						 	 </div> 
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="js/app.js?v=<?=microtime()?>"></script>
+</body>
+</html>
---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+$(window).scroll(function (event) {
+   	delegateScroll();
+});
+
+delegateScroll();
+function delegateScroll() {
+	$('section').each(function(index){
+		if(isScrolledIntoView('#sec'+index) && ! $('#sec'+index).hasClass('finish')) {
+			bringElems('#sec'+index);
+		}
+	});
+}
+
+function bringElems(section) {
+	$(section).addClass('finish');
+	$(section+' .anim').each(function(index){
+		setTimeout(function(){
+			$(section+' .anim_'+index).addClass('fadeInDown animated');
+		},1000*index);
+	});
+}
+
+function isScrolledIntoView(elem)
+{
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
